01cfea7847bccd544ba54d0b6c87ce08bdefc5d3
Pavankumar-01 <pavan.kreeekiit@gmail.com>
2020-07-16 21:40:41 +0530

    initial commit

---
+++ Project4PrimeNo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4PrimeNo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ctr= 0,i,n;
+            Console.WriteLine("\nEnter a number to check : ");
+            n = Convert.ToInt16(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    ctr++;
+                }
+            }
+            if (ctr == 2)
+            {
+                Console.WriteLine("{0} is a Prime Number", n);
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
